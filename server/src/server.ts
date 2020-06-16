9bf235a9beddd63f63d3fc35c91cb0c61126b6f6
vitormuuniz <vitormuuniz@gmail.com>
2020-06-16 09:24:28 -0300

    Last class

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import routes from "./routes";
 import cors from "cors";
-
+import { errors } from 'celebrate';
 const app = express();
 
 //set domains that are allowed to access the backend
@@ -14,8 +14,11 @@ app.use(express.json());
 //using routes from routes.ts
 app.use(routes);
 
-//creating a route for access the images
+//creating a route for access images
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+//for deal with erros
+app.use(errors());
 
 //listening on 3333 port
 app.listen(3333);
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import { LeafletMouseEvent } from "leaflet";
 import api from "../../services/api";
 import axios from "axios";
 
+import Dropzone from "../../components/Dropzone";
+
 import "./styles.css";
 
 import Logo from "../../assets/logo.svg";
@@ -39,9 +41,27 @@ const CreatePoint: React.FC = () => {
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
-  const [selectedPosition, setSelectedPosition] = useState<[number,number]>([0,0]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
 
   const history = useHistory();
+
+  //get current position
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   //when and what function to perform
   useEffect(() => {
@@ -112,31 +132,32 @@ const CreatePoint: React.FC = () => {
     }
   }
 
-  async function handleSubmit(event: FormEvent){
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
-    const { name, email, whatsapp }  =formData;
+    const { name, email, whatsapp } = formData;
     const uf = selectedUf;
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name, 
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    }
+    const data = new FormData();
 
-    await api.post('points', data);
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    if (selectedFile) data.append("image", selectedFile);
 
-    alert('Ponto de coleta criado!');
+    await api.post("points", data);
 
-    history.push('/');
+    alert("Ponto de coleta criado!");
+
+    history.push("/");
   }
 
   return (
@@ -153,6 +174,8 @@ const CreatePoint: React.FC = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
@@ -197,11 +220,7 @@ const CreatePoint: React.FC = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map
-            center={[-22.6906282, -46.7768741]}
-            zoom={15}
-            onClick={handleMapClick}
-          >
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
